6a4c2ad8eb2d0bd214ebc111ab6a792b0ef48d1d
Darío Rivera <fermius.us@gmail.com>
2020-10-03 00:01:57 -0500

    feat: add basic task list

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div id="app">
     <nav-bar></nav-bar>
+
+    <div class="container mt-5">
+      <h2>Todo List</h2>
+
+      <ul>
+        <li v-for="task in tasks" :key="task">{{ task.text }}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -11,6 +19,24 @@ export default {
   name: "App",
   components: {
     NavBar,
+  },
+  data: function() {
+    return {
+      tasks: [
+        {
+          text: "Task 1",
+          done: true,
+        },
+        {
+          text: "Task 2",
+          done: false,
+        },
+        {
+          text: "Task 3",
+          done: false,
+        },
+      ],
+    };
   },
 };
 </script>
