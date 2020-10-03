0df03b7a2fd8ec7cb4b0df6b842666a06f9b72c2
Darío Rivera <fermius.us@gmail.com>
2020-10-02 23:51:24 -0500

    fix: restore productionTip

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from "./App.vue";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+Vue.config.productionTip = false;
+
 new Vue({
   render: (h) => h(App),
 }).$mount("#app");
