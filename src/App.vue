1e172ad12a10488b20b4f776fde33cd41456a0f2
Darío Rivera <fermius.us@gmail.com>
2020-10-02 23:48:33 -0500

    feat: add bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <nav-bar></nav-bar>
+  </div>
 </template>
 
 <script>
+import NavBar from "./components/NavBarComponent";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    NavBar,
+  },
 };
 </script>
 
