e42aa445636b68a18e0ee49200616f4a12167483
Darío Rivera <fermius.us@gmail.com>
2020-10-07 22:02:45 -0500

    feat: add base button component

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <button
+    type="button"
+    :class="['btn', className]"
+    v-bind="$attrs"
+    @click="$emit('click', $event)"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    className: function() {
+      return `btn-${this.type}`;
+    },
+  },
+};
+</script>
